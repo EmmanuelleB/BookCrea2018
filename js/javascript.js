e906375907b8b3b687cba018a45ff3a2ab32bb89
Emmanuelle <emmanuellebaron1@gmail.com>
2018-05-16 08:21:45 +0200

    ajout pages a-propos.html, mes-travaux.html, mes-armes.html, contact.html, dossier css et js

---
+++ js/javascript.js
@@ -0,0 +1,85 @@
+  //   $(document).ready(function(){
+  //   $(".carrousel-description").hide();
+  //   $(".carrousel-section").click(function(){
+  //     $(this).next(".carrousel-description").slideDown(300).siblings(".carrousel-description").slideUp(300);
+  //   });
+  // });
+
+
+$(document).ready(function () {
+
+    //rotation speed and timer
+    var speed = 20000;
+
+    var run = setInterval(rotate, speed);
+    var slides = $('.slide');
+    var container = $('#slides ul');
+    var elm = container.find(':first-child').prop("tagName");
+    var item_width = container.width();
+    var previous = 'prev'; //id of previous button
+    var next = 'next'; //id of next button
+    slides.width(item_width); //set the slides to the correct pixel width
+    container.parent().width(item_width);
+    container.width(slides.length * item_width); //set the slides container to the correct total width
+    container.find(elm + ':first').before(container.find(elm + ':last'));
+    resetSlides();
+
+
+    //if user clicked on prev button
+
+    $('#buttons a').click(function (e) {
+        //slide the item
+        if ($(window).width()>480){
+
+        if (container.is(':animated')) {
+            return false;
+        }
+        if (e.target.id == previous) {
+            container.stop().animate({
+                'left': 0
+            }, 1500, function () {
+                container.find(elm + ':first').before(container.find(elm + ':last'));
+                resetSlides();
+            });
+        }
+
+        if (e.target.id == next) {
+            container.stop().animate({
+                'left': item_width * -2
+            }, 1500, function () {
+                container.find(elm + ':last').after(container.find(elm + ':first'));
+                resetSlides();
+            });
+        }
+
+        //cancel the link behavior
+        return false;
+      }
+    });
+
+    //if mouse hover, pause the auto rotation, otherwise rotate it
+    container.parent().mouseenter(function () {
+        clearInterval(run);
+    }).mouseleave(function () {
+        run = setInterval(rotate, speed);
+    });
+
+
+    function resetSlides() {
+        //and adjust the container so current is in the frame
+        container.css({
+            'left': -1 * item_width
+        });
+
+   }
+
+});
+//a simple function to click next link
+//a timer will call this function, and the rotation will begin
+
+function rotate() {
+    $('#next').click();
+}
+
+
+
